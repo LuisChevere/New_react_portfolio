918a61684163623c658e4c33cd74caaf42a58e04
Luis <hairchevere@gmail.com>
2026-08-26 10:12:50 -0500

    cleaned up Project component

--- my-portfolio/src/components/Projects.jsx
+++ my-portfolio/src/components/Projects.jsx
@@ -2,6 +2,33 @@ import Atelier from "../assets/images/Atelier.png";
 import LookBook from "../assets/images/Lookbook.png";
 import Heathenz from "../assets/images/Heathenz.png";
 
+const projects = [
+  {
+    title: "LookBook",
+    description: "Editorial Wordpress Site",
+    image: LookBook, // Update image path
+    link: "https://lookbook.luischevere.com/",
+  },
+  {
+    title: "Heathenz Entertainment",
+    description: "Brand Wordpress Website",
+    image: Heathenz, // Update image path
+    link: "https://heathenzentertainment.com/",
+  },
+  {
+    title: "LC Atelier",
+    description: "WooCommerce Clothing Store",
+    image: Atelier, // Update image path
+    link: "https://atelier.luischevere.com/",
+  },
+  {
+    title: "React Web Dev Portfolio",
+    description: "React portfolie Website",
+    image: Heathenz, // Update image path
+    link: "https://atelier.luischevere.com/",
+  },
+];
+
 function Projects() {
   return (
     <section className="bg-[#0A0A0A] px-[40px] py-[40px]" id="Projects">
@@ -11,169 +38,47 @@ function Projects() {
         </h2>
 
         <div className="flex justify-center gap-5">
-          <div className=" block max-w-70 border border-[#B88A46] rounded-base pb-[15px]">
-            <a>
-              <img src={LookBook} alt="Lookbook landing" />
-            </a>
-            <div class="pl-[10px]">
-              <span class=" bg-brand-softer border border-brand-subtle text-fg-brand-strong text-xs font-medium  rounded-sm"></span>
-              <a href="#">
-                <h5 class="font-['Cormorant_Garamond'] text-[15px] font-normal tracking-[2px] text-white leading-none pb-[10px] uppercase">
-                  lookbook
-                </h5>
-                <p className="text-white font-thin pb-[10px] text-[14px]">
-                  Editorial Wordpress Site
-                </p>
+          {projects.map((project) => (
+            <div className=" block max-w-70 border border-[#B88A46] rounded-base pb-[15px]">
+              <a key={project.image}>
+                <img src={project.image} alt="Lookbook landing" />
               </a>
-              <a
-                href="https://lookbook.luischevere.com/"
-                target="_blank"
-                class="inline-flex items-center text-[#B88A46] font-light hover:text-white leading-5 rounded-base text-sm focus:outline-none uppercase"
-              >
-                view project
-                <svg
-                  class="w-4 h-4 ms-1.5 rtl:rotate-180 -me-0.5"
-                  aria-hidden="true"
-                  xmlns="https://lookbook.luischevere.com/"
-                  width="24"
-                  height="24"
-                  fill="none"
-                  viewBox="0 0 24 24"
+              <div className="pl-[10px] pt-[20px]">
+                <a href="#hero">
+                  <h5 class="font-['Cormorant_Garamond'] text-[15px] font-normal tracking-[2px] text-white leading-none pb-[10px] uppercase">
+                    {project.title}
+                  </h5>
+                  <p className="text-white font-thin pb-[10px] text-[14px]">
+                    {project.description}
+                  </p>
+                </a>
+                <a
+                  href={project.link}
+                  target="_blank"
+                  class="inline-flex items-center text-[#B88A46] font-light hover:text-white leading-5 rounded-base text-sm focus:outline-none uppercase"
                 >
-                  <path
-                    stroke="currentColor"
-                    stroke-linecap="round"
-                    stroke-linejoin="round"
-                    stroke-width="2"
-                    d="M19 12H5m14 0-4 4m4-4-4-4"
-                  />
-                </svg>
-              </a>
+                  view project
+                  <svg
+                    class="w-4 h-4 ms-1.5 rtl:rotate-180 -me-0.5"
+                    aria-hidden="true"
+                    xmlns="https://lookbook.luischevere.com/"
+                    width="24"
+                    height="24"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                  >
+                    <path
+                      stroke="currentColor"
+                      stroke-linecap="round"
+                      stroke-linejoin="round"
+                      stroke-width="2"
+                      d="M19 12H5m14 0-4 4m4-4-4-4"
+                    />
+                  </svg>
+                </a>
+              </div>
             </div>
-          </div>
-
-          <div className=" block max-w-70 border border-[#B88A46] rounded-base pb-[15px]">
-            <a>
-              <img src={Heathenz} alt="Heathenz landing" />
-            </a>
-            <div class="pl-[10px]">
-              <span class="inline-flex bg-brand-softer border border-brand-subtle text-fg-brand-strong text-xs font-medium  rounded-sm"></span>
-              <a href="#">
-                <h5 class="font-['Cormorant_Garamond'] text-[15px] font-normal tracking-[2px] text-white leading-none pb-[10px] uppercase">
-                  heathenz entertainment
-                </h5>
-                <p className="text-white font-thin pb-[10px] text-[14px]">
-                  Brand Wordpress Website
-                </p>
-              </a>
-              <a
-                href="https://heathenzentertainment.com/"
-                target="_blank"
-                class="inline-flex items-center text-[#B88A46] font-light hover:text-white leading-5 rounded-base text-sm focus:outline-none uppercase"
-              >
-                view project
-                <svg
-                  class="w-4 h-4 ms-1.5 rtl:rotate-180 -me-0.5"
-                  aria-hidden="true"
-                  xmlns="https://lookbook.luischevere.com/"
-                  width="24"
-                  height="24"
-                  fill="none"
-                  viewBox="0 0 24 24"
-                >
-                  <path
-                    stroke="currentColor"
-                    stroke-linecap="round"
-                    stroke-linejoin="round"
-                    stroke-width="2"
-                    d="M19 12H5m14 0-4 4m4-4-4-4"
-                  />
-                </svg>
-              </a>
-            </div>
-          </div>
-
-          <div className=" block max-w-70 border border-[#B88A46] rounded-base pb-[15px]">
-            <a>
-              <img src={Atelier} alt="Lookbook landing" />
-            </a>
-            <div class="pl-[10px]">
-              <span class=" bg-brand-softer border border-brand-subtle text-fg-brand-strong text-xs font-medium  rounded-sm"></span>
-              <a href="#">
-                <h5 class="font-['Cormorant_Garamond'] text-[15px] font-normal tracking-[2px] text-white leading-none pb-[10px] uppercase">
-                  lc atelier
-                </h5>
-                <p className="text-white font-thin pb-[10px] text-[14px]">
-                  WooCommerce Store
-                </p>
-              </a>
-              <a
-                href="https://atelier.luischevere.com/"
-                target="_blank"
-                class="inline-flex items-center text-[#B88A46] font-light hover:text-white leading-5 rounded-base text-sm focus:outline-none uppercase"
-              >
-                view project
-                <svg
-                  class="w-4 h-4 ms-1.5 rtl:rotate-180 -me-0.5"
-                  aria-hidden="true"
-                  xmlns="https://atelier.luischevere.com/"
-                  width="24"
-                  height="24"
-                  fill="none"
-                  viewBox="0 0 24 24"
-                >
-                  <path
-                    stroke="currentColor"
-                    stroke-linecap="round"
-                    stroke-linejoin="round"
-                    stroke-width="2"
-                    d="M19 12H5m14 0-4 4m4-4-4-4"
-                  />
-                </svg>
-              </a>
-            </div>
-          </div>
-
-          <div className=" block max-w-70 border border-[#B88A46] rounded-base pb-[15px]">
-            <a>
-              <img src={Atelier} alt="Lookbook landing" />
-            </a>
-            <div class="pl-[10px]">
-              <span class=" bg-brand-softer border border-brand-subtle text-fg-brand-strong text-xs font-medium  rounded-sm"></span>
-              <a href="#">
-                <h5 class="font-['Cormorant_Garamond'] text-[15px] font-normal tracking-[2px] text-white leading-none pb-[10px] uppercase">
-                  lc atelier
-                </h5>
-                <p className="text-white font-thin pb-[10px] text-[14px]">
-                  WooCommerce Store
-                </p>
-              </a>
-              <a
-                href="https://atelier.luischevere.com/"
-                target="_blank"
-                class="inline-flex items-center text-[#B88A46] font-light hover:text-white leading-5 rounded-base text-sm focus:outline-none uppercase"
-              >
-                view project
-                <svg
-                  class="w-4 h-4 ms-1.5 rtl:rotate-180 -me-0.5"
-                  aria-hidden="true"
-                  xmlns="https://atelier.luischevere.com/"
-                  width="24"
-                  height="24"
-                  fill="none"
-                  viewBox="0 0 24 24"
-                >
-                  <path
-                    stroke="currentColor"
-                    stroke-linecap="round"
-                    stroke-linejoin="round"
-                    stroke-width="2"
-                    d="M19 12H5m14 0-4 4m4-4-4-4"
-                  />
-                </svg>
-              </a>
-            </div>
-          </div>
+          ))}
         </div>
       </div>
     </section>
