73fff93034da5d59673a43dbbafbf2c0cc5a3c52
Luis <hairchevere@gmail.com>
2026-08-26 10:52:12 -0500

    Const/Mapping for Skills Component to clean up HTML

--- my-portfolio/src/components/Skills.jsx
+++ my-portfolio/src/components/Skills.jsx
@@ -4,6 +4,49 @@ import Wordpressgold from "../assets/logos/Wordpress-gold.png";
 import Cart from "../assets/logos/Cart.png";
 import Tools from "../assets/logos/Tools.png";
 
+const skills = [
+  {
+    title: "FrontEnd",
+    skill1: "React",
+    skill2: "Javascript(ES6+)",
+    skill3: "HTML5 / CSS",
+    skill4: "Tailwind CSS",
+    image: Frontend,
+  },
+  {
+    title: "Backend",
+    skill1: "Node",
+    skill2: "RESTfull APIs",
+    skill3: "Python",
+    skill4: "PHP (Novice)",
+    image: Backend,
+  },
+  {
+    title: "E-Commerce",
+    skill1: "Custom Theme",
+    skill2: "Elementor",
+    skill3: "WooCommerce",
+    skill4: "ACF / Custom Fields",
+    image: Cart,
+  },
+  {
+    title: "Wordpress",
+    skill1: "WooCommmerce",
+    skill2: "Shopify (Learning)",
+    skill3: "Product Management",
+    skill4: "Payment Integration",
+    image: Wordpressgold,
+  },
+  {
+    title: "Tools",
+    skill1: "Git & GitHub",
+    skill2: "VS Code",
+    skill3: "Chrome DevTools",
+    skill4: "Photoshop",
+    image: Tools,
+  },
+];
+
 function Skills() {
   return (
     <section
@@ -16,93 +59,45 @@ function Skills() {
         </h2>
       </div>
       <div className="flex gap-10 pl-[40px]">
-        <div className="gap-1">
-          <a className="flex gap-3">
-            <img className="max-w-10" src={Frontend} alt="FrontEnd Logo" />
-            <h4 className="text-white text-[14px] uppercase pt-[6px] tracking-[2px]">
-              frontend
-            </h4>
-          </a>
-          <div className="text-white">
-            <ul class="list-disc list-outside pl-5 space-y-2 text-[13px] pt-[15px] pl-[70px] tracking-[1px]">
-              <li>React</li>
-              <li>Javascript(ES6+)</li>
-              <li>HTML5 / CSS</li>
-              <li>Tailwind CSS</li>
-            </ul>
-          </div>
-        </div>
+        {skills.map((skill) => (
+          <div className="gap-1">
+            <a className="flex gap-3" key={skill.image}>
+              <img className="max-w-12" src={skill.image} alt="FrontEnd Logo" />
+              <h4 className="text-white text-[14px] uppercase pt-[6px] tracking-[2px]">
+                {skill.title}
+              </h4>
+            </a>
 
-        <div className="gap-1">
-          <a className="flex gap-3">
-            <img className="max-w-10" src={Backend} alt="FrontEnd Logo" />
-            <h4 className="text-white text-[14px] uppercase pt-[6px] tracking-[2px]">
-              backend
-            </h4>
-          </a>
-          <div className="text-white">
-            <ul class="list-disc list-outside pl-5 space-y-2 text-[13px] pt-[15px] pl-[70px] tracking-[1px]">
-              <li>Node</li>
-              <li>RESTfull APIs</li>
-              <li>Python</li>
-              <li>PHP (Novice)</li>
-            </ul>
+            <div className="text-white">
+              <ul class="list-disc list-outside pl-5 space-y-2 text-[13px] pt-[15px] pl-[70px] tracking-[1px]">
+                <li>{skill.skill1}</li>
+                <li>{skill.skill2}</li>
+                <li>{skill.skill3}</li>
+                <li>{skill.skill4}</li>
+              </ul>
+            </div>
           </div>
-        </div>
-
-        <div className="gap-1">
-          <a className="flex gap-3">
-            <img className="max-w-10" src={Wordpressgold} alt="FrontEnd Logo" />
-            <h4 className="text-white text-[14px] uppercase pt-[6px] tracking-[2px]">
-              wordpress
-            </h4>
-          </a>
-          <div className="text-white">
-            <ul class="list-disc list-outside pl-5 space-y-2 text-[13px] pt-[15px] pl-[70px] tracking-[1px]">
-              <li>Custom Themes</li>
-              <li>Elementor</li>
-              <li>WooCommerce</li>
-              <li>ACF / Custom Fields</li>
-            </ul>
-          </div>
-        </div>
-
-        <div className="gap-1">
-          <a className="flex gap-3">
-            <img className="max-w-10" src={Cart} alt="FrontEnd Logo" />
-            <h4 className="text-white text-[14px] uppercase pt-[6px] tracking-[2px]">
-              e-commerce
-            </h4>
-          </a>
-          <div className="text-white">
-            <ul class="list-disc list-outside pl-5 space-y-2 text-[13px] pt-[15px] pl-[70px] tracking-[1px]">
-              <li>Woocommerce</li>
-              <li>Shopify (Learning)</li>
-              <li>Product Management</li>
-              <li>Payment Integration</li>
-            </ul>
-          </div>
-        </div>
-
-        <div className="gap-1">
-          <a className="flex gap-3">
-            <img className="max-w-10" src={Tools} alt="FrontEnd Logo" />
-            <h4 className="text-white text-[14px] uppercase pt-[6px] tracking-[2px]">
-              tools
-            </h4>
-          </a>
-          <div className="text-white">
-            <ul class="list-disc list-outside pl-5 space-y-2 text-[13px] pt-[15px] pl-[70px] tracking-[1px]">
-              <li>Git & GitHub</li>
-              <li>Photoshop</li>
-              <li>VS Code</li>
-              <li>Chrome DevTools</li>
-            </ul>
-          </div>
-        </div>
+        ))}
       </div>
     </section>
   );
 }
 
 export default Skills;
+
+//         <div className="gap-1">
+//           <a className="flex gap-3">
+//             <img className="max-w-10" src={Tools} alt="FrontEnd Logo" />
+//             <h4 className="text-white text-[14px] uppercase pt-[6px] tracking-[2px]">
+//               tools
+//             </h4>
+//           </a>
+//           <div className="text-white">
+//             <ul class="list-disc list-outside pl-5 space-y-2 text-[13px] pt-[15px] pl-[70px] tracking-[1px]">
+//               <li>Git & GitHub</li>
+//               <li>Photoshop</li>
+//               <li>VS Code</li>
+//               <li>Chrome DevTools</li>
+//             </ul>
+//           </div>
+//         </div>
