620362635326a3a4a4fcc382020afd69a419bc33
Luis <hairchevere@gmail.com>
2026-08-27 12:51:50 -0500

    updated socials in Navbar to follow same pattern as other links and added social sites to logos

--- my-portfolio/src/components/Navbar.jsx
+++ my-portfolio/src/components/Navbar.jsx
@@ -4,6 +4,27 @@ import LinkedIn from "../assets/logos/LinkedIn.png";
 import Instagram from "../assets/logos/Instagram.png";
 import Email from "../assets/logos/Mail.png";
 
+const socials = [
+  {
+    title: "Github",
+    description: "Github link",
+    image: Github,
+    link: "https://github.com/LuisChevere",
+  },
+  {
+    title: "LinkedIn",
+    description: "LinkedIn link",
+    image: LinkedIn,
+    link: "https://www.linkedin.com/in/luis-chevere-4a417575",
+  },
+  {
+    title: "Instagram",
+    description: "Instagram link",
+    image: Instagram,
+    link: "https://www.instagram.com/a.crowleyy_/?hl=en",
+  },
+];
+
 function NavBar() {
   return (
     <nav className="flex pl-10 pr-5 py-5 text-white text-[12px]">
@@ -54,12 +75,17 @@ function NavBar() {
           </a>
         </li>
       </ul>
-      <li className="inline-flex space-x-5 pt-[12px]">
-        <img className="w-5 h-5" src={Github} href="" alt="Github llink" />
-        <img className="w-5 h-5" src={LinkedIn} href="" alt="LinkedIN link" />
-        <img className="w-5 h-5" src={Instagram} href="" alt="Instagram link" />
-        <img className="w-6 h-5" src={Email} href="" alt="Email Link" />
-      </li>
+
+      <div className="inline-flex space-x-5 pt-[12px]">
+        {socials.map((social) => (
+          <a href={social.link} target="blank">
+            <img className="w-5 h-5" src={social.image} />
+          </a>
+        ))}
+        <a target="blank">
+          <img className="w-6 h-5" src={Email} />
+        </a>
+      </div>
     </nav>
   );
 }
